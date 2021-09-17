0587fcba0ecb2bc08b538ee8fbe5d01c3a607296
rzdzaky <76608040+rzdzaky@users.noreply.github.com>
2021-09-17 21:46:14 +0700

    Menambahkan method penumpang naik

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ function Angkot(supir,trayek,penumpang,kas) {
     this.trayek = trayek;
     this.penumpang = penumpang;
     this.kas = kas;
+
+    this.penumpangNaik = function(namaPenumpang) {
+        this.penumpang.push(namaPenumpang);
+        return this.penumpang;
+    }
 }
